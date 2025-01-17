4d3012e43d3d401ec7c12e6a509019d97e739d41
Marcinp98 <marcinpilipczuk98@gmail.com>
2025-01-17 14:41:16 +0100

    Add CharacterBase, MainCharacter, EnemyBase classes

---
+++ Source/SideScrollerGame/SideScrollerGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SideScrollerGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SideScrollerGame, "SideScrollerGame" );
