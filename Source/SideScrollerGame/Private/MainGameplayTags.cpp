7da90bd06c5e78d31cd1d55b2ebc3bbdfe0bc385
Marcinp98 <marcinpilipczuk98@gmail.com>
2025-03-21 15:46:50 +0100

    Add ability cooldown

--- Source/SideScrollerGame/Private/MainGameplayTags.cpp
+++ Source/SideScrollerGame/Private/MainGameplayTags.cpp
@@ -89,6 +89,10 @@ void FMainGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.Abilities_Attack = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Attack"), FString("Attack Ability Tag"));
 	GameplayTags.Abilities_Summon = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Summon"), FString("Summon Ability Tag"));
 	GameplayTags.Abilities_Fire_FireBolt = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Fire.FireBolt"), FString("FireBolt Ability Tag"));
+	
+	/* Cooldown */
+
+	GameplayTags.Cooldown_Fire_FireBolt = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Cooldown.Fire.FireBolt"), FString("Firebolt Cooldown Tag"));
 
 	/* CombatSockets */
 
